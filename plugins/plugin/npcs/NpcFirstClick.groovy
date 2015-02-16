49a5c2f39a021c69af14929eff5e1c371a9d04cf
lare96 <lare96@live.com>
2015-02-16 17:48:41 -0500

    Added an NPC at home that will take you to the fight pits minigame.

--- plugins/plugin/npcs/NpcFirstClick.groovy
+++ plugins/plugin/npcs/NpcFirstClick.groovy
@@ -2,11 +2,11 @@ package plugin.npcs
 
 import java.util.function.Consumer
 
+import plugin.minigames.FightCavesHandler
 import plugin.skills.fishing.Fishing
 import plugin.skills.fishing.Tool
 
 import com.asteria.game.character.player.Player
-import com.asteria.game.character.player.dialogue.DialogueChainBuilder
 import com.asteria.game.character.player.dialogue.GiveItemDialogue
 import com.asteria.game.character.player.dialogue.NpcDialogue
 import com.asteria.game.character.player.dialogue.OptionDialogue
@@ -14,8 +14,8 @@ import com.asteria.game.character.player.dialogue.OptionType
 import com.asteria.game.character.player.dialogue.PlayerDialogue
 import com.asteria.game.item.Item
 import com.asteria.game.location.Position
-import com.asteria.game.plugin.PluginSignature
 import com.asteria.game.plugin.PluginListener
+import com.asteria.game.plugin.PluginSignature
 import com.asteria.game.plugin.context.NpcFirstClickPlugin
 import com.asteria.game.shop.Shop
 
@@ -28,12 +28,11 @@ final class NpcFirstClick implements PluginListener<NpcFirstClickPlugin> {
         Position position = context.npc.position.copy()
         switch (id) {
             case 460:
-                DialogueChainBuilder builder = player.dialogueChain
-                builder.append new NpcDialogue(id, "Hello, this is the new dialogue system. It",
-                        "should be easier to use.")
-                builder.append new PlayerDialogue("Cool, I'm loving it already!")
-                builder.append new NpcDialogue(id, "Would you like some money?")
-                builder.append new OptionDialogue(["Yes please!", "No thanks."]) {
+                player.dialogueChain.append new NpcDialogue(id, "Hello, this is the new dialogue system. It",
+                "should be easier to use.")
+                player.dialogueChain.append new PlayerDialogue("Cool, I'm loving it already!")
+                player.dialogueChain.append new NpcDialogue(id, "Would you like some money?")
+                player.dialogueChain.append new OptionDialogue(["Yes please!", "No thanks."]) {
                     @Override
                     public Optional<Consumer<OptionType>> getOptionListener() {
                         return Optional
@@ -41,9 +40,9 @@ final class NpcFirstClick implements PluginListener<NpcFirstClickPlugin> {
                                     @Override
                                     public void accept(OptionType t) {
                                         if (t == OptionType.FIRST_OPTION) {
-                                            builder.append new GiveItemDialogue(new Item(995, 1000),
+                                            player.dialogueChain.append new GiveItemDialogue(new Item(995, 1000),
                                                     "You receive 1000 gold coins.")
-                                            builder.advance()
+                                            player.dialogueChain.advance()
                                         } else if (t == OptionType.SECOND_OPTION) {
                                             player.encoder.sendCloseWindows()
                                         }
@@ -51,7 +50,27 @@ final class NpcFirstClick implements PluginListener<NpcFirstClickPlugin> {
                                 })
                     }
                 }
-                builder.advance()
+                player.dialogueChain.advance()
+                break
+            case 2617:
+                player.dialogueChain.append new NpcDialogue(id, "Greetings, would you like to be taken to the", "fight pits minigame?")
+                player.dialogueChain.append new OptionDialogue(["Yes, take me!", "I'd rather not."]) {
+                    @Override
+                    public Optional<Consumer<OptionType>> getOptionListener() {
+                        return Optional
+                                .of(new Consumer<OptionType>() {
+                                    @Override
+                                    public void accept(OptionType t) {
+                                        if (t == OptionType.FIRST_OPTION) {
+                                            player.move FightCavesHandler.DEATH_POSITION
+                                        } else if (t == OptionType.SECOND_OPTION) {
+                                            player.encoder.sendCloseWindows()
+                                        }
+                                    }
+                                })
+                    }
+                }
+                player.dialogueChain.advance()
                 break
             case 520:
                 Shop.SHOPS.get("General Store").openShop player
