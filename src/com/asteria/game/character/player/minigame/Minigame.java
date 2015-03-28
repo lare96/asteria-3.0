abb706670be040b7982f300b02b0211ca79a7ae0
lare96 <lare96@live.com>
2015-03-28 13:12:16 -0400

    Formatted the Minigame class.

--- src/com/asteria/game/character/player/minigame/Minigame.java
+++ src/com/asteria/game/character/player/minigame/Minigame.java
@@ -12,7 +12,7 @@ import com.asteria.game.plugin.PluginContext;
  * The class that provides all of the functionality needed for very generic
  * minigames. These types of minigames can usually be ran on their own meaning
  * they aren't dependent on some sort of sequencer or task.
- * 
+ *
  * @author lare96 <http://github.com/lare96>
  */
 public abstract class Minigame implements PluginContext {
@@ -29,11 +29,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Creates a new {@link Minigame}.
-     * 
+     *
      * @param name
-     *            the current name of this minigame.
+     *         the current name of this minigame.
      * @param type
-     *            the current type of this minigame;
+     *         the current type of this minigame;
      */
     public Minigame(String name, MinigameType type) {
         this.name = name;
@@ -60,18 +60,19 @@ public abstract class Minigame implements PluginContext {
         if (name == null) {
             if (other.name != null)
                 return false;
-        } else if (!name.equals(other.name))
+        }
+        else if (!name.equals(other.name))
             return false;
         return true;
     }
 
     /**
      * The method executed when the first click npc packet is sent.
-     * 
+     *
      * @param player
-     *            the player the packet was sent for.
+     *         the player the packet was sent for.
      * @param npc
-     *            the npc that was clicked by the player.
+     *         the npc that was clicked by the player.
      */
     public void onFirstClickNpc(Player player, Npc npc) {
 
@@ -79,11 +80,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * The method executed when the second click npc packet is sent.
-     * 
+     *
      * @param player
-     *            the player the packet was sent for.
+     *         the player the packet was sent for.
      * @param npc
-     *            the npc that was clicked by the player.
+     *         the npc that was clicked by the player.
      */
     public void onSecondClickNpc(Player player, Npc npc) {
 
@@ -91,13 +92,13 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * The method executed when the first click object packet is sent.
-     * 
+     *
      * @param player
-     *            the player the packet was sent for.
+     *         the player the packet was sent for.
      * @param id
-     *            the identification of the object that was clicked.
+     *         the identification of the object that was clicked.
      * @param position
-     *            the position that the clicked object is on.
+     *         the position that the clicked object is on.
      */
     public void onFirstClickObject(Player player, int id, Position position) {
 
@@ -105,13 +106,13 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * The method executed when the second click object packet is sent.
-     * 
+     *
      * @param player
-     *            the player the packet was sent for.
+     *         the player the packet was sent for.
      * @param id
-     *            the identification of the object that was clicked.
+     *         the identification of the object that was clicked.
      * @param position
-     *            the position that the clicked object is on.
+     *         the position that the clicked object is on.
      */
     public void onSecondClickObject(Player player, int id, Position position) {
 
@@ -119,13 +120,13 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * The method executed when the third click object packet is sent.
-     * 
+     *
      * @param player
-     *            the player the packet was sent for.
+     *         the player the packet was sent for.
      * @param id
-     *            the identification of the object that was clicked.
+     *         the identification of the object that was clicked.
      * @param position
-     *            the position that the clicked object is on.
+     *         the position that the clicked object is on.
      */
     public void onThirdClickObject(Player player, int id, Position position) {
 
@@ -133,11 +134,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * The method executed when {@code player} kills {@code other}.
-     * 
+     *
      * @param player
-     *            the player that killed another character.
+     *         the player that killed another character.
      * @param other
-     *            the character that was killed by the player.
+     *         the character that was killed by the player.
      */
     public void onKill(Player player, CharacterNode other) {
 
@@ -145,9 +146,9 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * The method executed when {@code player} dies.
-     * 
+     *
      * @param player
-     *            the player that died within the minigame.
+     *         the player that died within the minigame.
      */
     public void onDeath(Player player) {
 
@@ -156,15 +157,15 @@ public abstract class Minigame implements PluginContext {
     /**
      * Determines if {@code player} can equip {@code item} to
      * {@code equipmentSlot}.
-     * 
+     *
      * @param player
-     *            the player attempting to equip the item.
+     *         the player attempting to equip the item.
      * @param item
-     *            the item that is being equipped.
+     *         the item that is being equipped.
      * @param equipmentSlot
-     *            the index that the item is being equipped to.
+     *         the index that the item is being equipped to.
      * @return {@code true} if the player can equip the item, {@code false}
-     *         otherwise.
+     * otherwise.
      */
     public boolean canEquip(Player player, Item item, int equipmentSlot) {
         return true;
@@ -173,15 +174,15 @@ public abstract class Minigame implements PluginContext {
     /**
      * Determines if {@code player} can unequip {@code item} from
      * {@code equipmentSlot}.
-     * 
+     *
      * @param player
-     *            the player attempting to unequip the item.
+     *         the player attempting to unequip the item.
      * @param item
-     *            the item that is being unequipped.
+     *         the item that is being unequipped.
      * @param equipmentSlot
-     *            the index that the item is being unequipped from.
+     *         the index that the item is being unequipped from.
      * @return {@code true} if the player can equip the item, {@code false}
-     *         otherwise.
+     * otherwise.
      */
     public boolean canUnequip(Player player, Item item, int equipmentSlot) {
         return true;
@@ -189,11 +190,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Determines if {@code player} can trade {@code other}.
-     * 
+     *
      * @param player
-     *            the player attempting to trade the other player.
+     *         the player attempting to trade the other player.
      * @param other
-     *            the player that is being traded with.
+     *         the player that is being traded with.
      * @return {@code true} if the players can trade, {@code false} otherwise.
      */
     public boolean canTrade(Player player, Player other) {
@@ -203,11 +204,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Determines if {@code player} can attack {@code other}.
-     * 
+     *
      * @param player
-     *            the player attempting to attack the character.
+     *         the player attempting to attack the character.
      * @param other
-     *            the character that is being targeted.
+     *         the character that is being targeted.
      * @return {@code true} if the player can attack, {@code false} otherwise.
      */
     public boolean canHit(Player player, CharacterNode other) {
@@ -216,9 +217,9 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Determines if {@code player} can log out using the logout button.
-     * 
+     *
      * @param player
-     *            the player attempting to logout.
+     *         the player attempting to logout.
      * @return {@code true} if the player can logout, {@code false} otherwise.
      */
     public boolean canLogout(Player player) {
@@ -228,11 +229,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Determines if {@code player} can teleport somewhere.
-     * 
+     *
      * @param player
-     *            the player attempting to teleport.
+     *         the player attempting to teleport.
      * @param position
-     *            the destination the player is teleporting to.
+     *         the destination the player is teleporting to.
      * @return {@code true} if the player can teleport, {@code false} otherwise.
      */
     public boolean canTeleport(Player player, Position position) {
@@ -242,11 +243,11 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Determines if {@code player} can keep their items on death.
-     * 
+     *
      * @param player
-     *            the player attempting to keep their items.
+     *         the player attempting to keep their items.
      * @return {@code true} if the player can keep their items, {@code false}
-     *         otherwise.
+     * otherwise.
      */
     public boolean canKeepItems(Player player) {
         return true;
@@ -254,9 +255,9 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Retrieves the position {@code player} will be moved to when they respawn.
-     * 
+     *
      * @param player
-     *            the player who is being respawned.
+     *         the player who is being respawned.
      * @return the position the player will be moved to on death.
      */
     public Position deathPosition(Player player) {
@@ -266,34 +267,34 @@ public abstract class Minigame implements PluginContext {
     /**
      * The method executed when {@code player} has logged in while in the
      * minigame.
-     * 
+     *
      * @param player
-     *            the player that has logged in.
+     *         the player that has logged in.
      */
     public abstract void onLogin(Player player);
 
     /**
      * The method executed when {@code player} has disconnected while in the
      * minigame.
-     * 
+     *
      * @param player
-     *            the player that has logged out.
+     *         the player that has logged out.
      */
     public abstract void onLogout(Player player);
 
     /**
      * Determines if {@code player} is in this minigame.
-     * 
+     *
      * @param player
-     *            the player to determine this for.
+     *         the player to determine this for.
      * @return {@code true} if this minigame contains the player, {@code false}
-     *         otherwise.
+     * otherwise.
      */
     public abstract boolean contains(Player player);
 
     /**
      * Gets the current name of this minigame.
-     * 
+     *
      * @return the name of this minigame.
      */
     public final String getName() {
@@ -302,7 +303,7 @@ public abstract class Minigame implements PluginContext {
 
     /**
      * Gets the current type of this minigame.
-     * 
+     *
      * @return the type of this minigame.
      */
     public final MinigameType getType() {
