d8856f5cf97e6e2a1b357b2d8251d217339edf51
lare96 <lare96@live.com>
2015-04-11 20:49:20 -0400

    Added more logger utility functions.

--- src/com/asteria/utility/LoggerUtils.java
+++ src/com/asteria/utility/LoggerUtils.java
@@ -13,7 +13,7 @@ public final class LoggerUtils {
      * The default constructor.
      *
      * @throws UnsupportedOperationException
-     *         if this class is instantiated.
+     *             if this class is instantiated.
      */
     private LoggerUtils() {
         throw new UnsupportedOperationException("This class cannot be " + "instantiated!");
@@ -24,10 +24,32 @@ public final class LoggerUtils {
      * {@code clazz}.
      *
      * @param clazz
-     *         the class to create the new logger with.
+     *            the class to create the new logger with.
      * @return the logger instance.
      */
     public static Logger getLogger(Class<?> clazz) {
         return Logger.getLogger(clazz.getSimpleName());
+    }
+
+    /**
+     * Creates a new logger instance using {@code Asteria317} as the name.
+     * Please note that this method <b>does not</b> return a static-logger
+     * (because it wouldn't be thread safe), meaning that when this is used the
+     * logger instance should still be cached. An example below:
+     * <p>
+     * <p>
+     * 
+     * <pre>
+     * Logger log = Logger.getGlobal(); // Get a new logger, cache it.
+     * 
+     * // And now we can use it wherever.
+     * log.info(&quot;Hello, World!&quot;);
+     * log.warning(&quot;Uh oh, a warning!&quot;);
+     * </pre>
+     *
+     * @return the logger instance.
+     */
+    public static Logger getGlobal() {
+        return Logger.getLogger("Asteria317");
     }
 }
